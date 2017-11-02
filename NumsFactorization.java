94ec5ef7334c9bb6ba21bfc1801965229ca50756
Maxim Kostin <x035cm@mail.ru>
2017-11-03 01:38:30 +0300

    Update NumsFactorization.java

--- NumsFactorization.java
+++ NumsFactorization.java
@@ -3,12 +3,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-//The fundamental theorem of arithmetic, 
-//also called the unique factorization theorem or the unique-prime-factorization theorem, 
-//states that every integer greater than 1 either is prime itself
-//or is the product of prime numbers, and that this product is unique,
-//up to the order of the factors.
-//https://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic
 
 public class NumsFactorization {
 
